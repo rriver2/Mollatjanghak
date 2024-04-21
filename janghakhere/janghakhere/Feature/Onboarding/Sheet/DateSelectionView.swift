5519e995b3a6858e5932a0e966dfbd198ddd1d3e
Taehwan Kim <obtusa07@gmail.com>
2024-04-21 21:29:22 +0900

    [Feat] DateSelectionView 완성

---
+++ janghakhere/janghakhere/Feature/Onboarding/Sheet/DateSelectionView.swift
@@ -0,0 +1,38 @@
+//
+//  DateSelectionView.swift
+//  janghakhere
+//
+//  Created by Taehwan Kim on 4/21/24.
+//
+
+import SwiftUI
+
+struct DateSelectionView: View {
+    @Binding var date: Date
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack {
+            Text("날짜 선택")
+                .font(.title_xsm)
+                .padding()
+            Spacer()
+            DatePicker("날짜선택피커", selection: $date, displayedComponents: [.date])
+                .datePickerStyle(.wheel)
+                .labelsHidden()
+                .environment(\.locale, .init(identifier: "ko_KR"))
+            Spacer()
+            Button {
+                dismiss()
+            } label: {
+                Text("확인")
+            }
+            .buttonStyle(MainButtonStyle())
+            .padding(.horizontal, 31)
+        }
+        .presentationDetents([.medium])
+    }
+}
+//#Preview {
+//    DateSelectionView()
+//}
