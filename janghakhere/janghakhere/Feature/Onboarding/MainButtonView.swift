3b6a560777390d347a7770a49616f8e33f3be93b
Taehwan Kim <obtusa07@gmail.com>
2024-04-22 13:49:08 +0900

    [Feat] Main 버튼에 disabled 상태 삽입

--- janghakhere/janghakhere/Feature/Onboarding/MainButtonView.swift
+++ janghakhere/janghakhere/Feature/Onboarding/MainButtonView.swift
@@ -9,45 +9,48 @@ import SwiftUI
 
 struct MainButtonView: View {
     private let title: String
-    private let clickedButton: (() -> Void)
+    private let action: (() -> Void)
+    private var disabled: Bool = false
     
-    init(title: String, clickedButton: @escaping () -> Void) {
+    init(title: String, action: @escaping () -> Void, disabled: Bool = false) {
         self.title = title
-        self.clickedButton = clickedButton
+        self.action = action
+        self.disabled = disabled
     }
     
     var body: some View {
         Button {
-            clickedButton()
+            action()
         } label: {
             Text(title)
-                .font(.title_xsm)
-                .padding(.vertical, 16)
-                .foregroundStyle(.white)
-                .frame(maxWidth: .infinity)
-                .background(Color.mainGray)
-                .cornerRadius(100)
-                .padding(.horizontal, 11)
         }
+        .buttonStyle(MainButtonStyle(disabled: disabled))
+        .disabled(disabled)
     }
 }
 
 struct MainButtonStyle: ButtonStyle {
+    let disabled: Bool
+    
   func makeBody(configuration: Configuration) -> some View {
     configuration.label
       .font(.title_xsm)
       .padding(.vertical, 16)
-      .foregroundStyle(.white)
+      .foregroundStyle(disabled ? .black : .white)
       .frame(maxWidth: .infinity)
       .frame(height: 58)
       .background(
         Capsule()
-          .fill(.mainGray)
+            .fill(disabled ? .gray70 : .mainGray)
       )
-      .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
+      .scaleEffect(
+        configuration.isPressed && !disabled
+        ? 0.95
+        : 1.0
+      )
   }
 }
 
 #Preview {
-    MainButtonView(title: "다음", clickedButton: {})
+    MainButtonView(title: "다음", action: {})
 }
