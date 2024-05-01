011d05b2f783a608583340705b67890e7163a2d3
Taehwan Kim <obtusa07@gmail.com>
2024-05-01 15:32:02 +0900

    [Feat] 기타 뷰를 제외한 내 정보 화면 UI 구성

--- janghakhere/janghakhere/Feature/MyPage/MyInfomation/MyInformationViewModel.swift
+++ janghakhere/janghakhere/Feature/MyPage/MyInfomation/MyInformationViewModel.swift
@@ -10,10 +10,20 @@ import SwiftUI
 @MainActor
 final class MyInformationViewModel: ObservableObject {
     let managerActor: MyInformationActor = MyInformationActor()
+    @Published var name: String = "윤영서"
+    @Published var sex: Sex = .notSelected
+    @Published var militaryStatus: MilitaryStatus = .notSelected
+    @Published var incomeStatus: IncomeDecile = .notSelected
+    @Published var sibilingStatus: SiblingStatus = .notSelected
+    @Published var schoolName: String = "서울여자대학교"
+    @Published var semesterYear: SemesterYear = .sophomore
+    @Published var majorField: MajorField = .engineering
+    @Published var totalScore: String = "4.5"
     
     private var tasks: [Task<Void, Never>] = []
     
     @Published private(set) var defaultDatas: [String] = []
+    
 }
 
 // MARK: - private 함수들
