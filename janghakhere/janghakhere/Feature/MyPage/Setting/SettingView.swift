b6bae876c3e0adf2a4c748f202ca512ad27e9369
Taehwan Kim <obtusa07@gmail.com>
2024-05-02 22:54:56 +0900

    [Feat] SettingView 이메일 작성 기능 연결

--- janghakhere/janghakhere/Feature/MyPage/Setting/SettingView.swift
+++ janghakhere/janghakhere/Feature/MyPage/Setting/SettingView.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import MessageUI
 
 struct SettingView: View {
     @EnvironmentObject private var pathModel: PathModel
+    @State private var result: Result<MFMailComposeResult, Error>? = nil
+    @State private var isShowingMailView = false
+
     
     var body: some View {
         VStack(spacing: 0) {
@@ -17,7 +21,7 @@ struct SettingView: View {
             VStack(spacing: 0) {
                 settingCell(title: "문의하기")
                     .onTapGesture {
-                        pathModel.paths.append(.settingWebView(title: "문의하기", url: inquiryURL))
+                        isShowingMailView.toggle()
                     }
                 settingCell(title: "오픈소스 라이선스")
                     .onTapGesture {
@@ -31,6 +35,22 @@ struct SettingView: View {
             .paddingHorizontal()
             
             Spacer()
+        }
+        .sheet(isPresented: $isShowingMailView) {
+            MailView(result: $result) { composer in
+                composer.setSubject("여깄장학 문의")
+                let messageBody = """
+------------------
+- 유저: UserDefaults에 있는 유저 스트링
+- 일시:
+- iOS:
+- 기종:
+------------------
+
+"""
+                composer.setMessageBody(messageBody, isHTML: false)
+                composer.setToRecipients(["janghakhere@gmail.com"])
+            }
         }
         .navigationBarBackButtonHidden()
     }
@@ -47,6 +67,14 @@ extension SettingView {
         }
         .padding(.vertical, 19)
         .contentShape(Rectangle())
+    }
+    
+    func tapAskButton() {
+        if MFMailComposeViewController.canSendMail() {
+            isShowingMailView.toggle()
+        } else {
+            // TODO: 사용자의 이메일이 등록되어 있지 않는 경우 아무런 상호작용 발생하지 않음. 팝업? 어떤 식으로 알려주는 과정이 필요
+        }
     }
 }
 
