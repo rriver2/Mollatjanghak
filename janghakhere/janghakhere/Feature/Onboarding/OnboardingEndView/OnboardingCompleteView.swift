69450805fe2b423f85035636f551aac5439752f6
Taehwan Kim <obtusa07@gmail.com>
2024-05-06 02:44:33 +0900

    [Feat] OnboadingCompletion에 대한 로직, 변경사항 반영

--- janghakhere/janghakhere/Feature/Onboarding/OnboardingEndView/OnboardingCompleteView.swift
+++ janghakhere/janghakhere/Feature/Onboarding/OnboardingEndView/OnboardingCompleteView.swift
@@ -8,24 +8,32 @@
 import SwiftUI
 
 struct OnboardingCompleteView: View {
+    let count: Int
+    @EnvironmentObject private var pathModel: PathModel
+    
     @State var disable: Bool = false
     var body: some View {
         VStack(alignment: .center, spacing: 0) {
             HStack {
                 Spacer()
-                Button {} label: {
-                    Image(systemName: "xmark")
-                        .foregroundStyle(.mainGray)
-                        .font(.system(size: 28))
+                Button {
+                    withAnimation {
+                        pathModel.paths.append(.tapView)
+                    }
+                } label: {
+                    Icon(
+                        name: .exit,
+                        color: .mainGray,
+                        size: 28
+                    )
                 }
             }
             .padding(.vertical, 28)
             
-            Image(systemName: "bell")
-                .resizable()
-                .frame(width: 122, height: 122)
+            Icon(name: .hooray, size: 122)
                 .padding(.top, 40)
                 .padding(.bottom, 8)
+            
             Text("환영합니다!")
                 .font(.title_lg)
                 .foregroundStyle(.black)
@@ -34,31 +42,33 @@ struct OnboardingCompleteView: View {
                 .font(.title_sm)
                 .foregroundStyle(.gray600)
                 .padding(.bottom, 12)
-            HStack(spacing: 5) {
+            HStack(alignment: .top, spacing: 8) {
+                SlotMachineView(finalNumberList: separateDigits(count))
                 Text("개")
                     .font(.title_sm)
+                    .padding(.top, 6)
                     .foregroundStyle(.gray600)
             }
             Spacer()
-            Text("추가정보를 입력하면\n더 적절한 장학금을 추천드릴수 있어요")
-                .font(.text_md)
-                .foregroundStyle(.gray500)
-                .multilineTextAlignment(.center)
-                .padding(.vertical, 20)
             
             NonMaxButton(
-                title: "더 입력할래요",
+                title: "공고 보러가기",
                 action: {
-                    
+                    withAnimation {
+                        pathModel.paths.append(.tapView)
+                    }
                 }
             )
-            
-            .padding(.horizontal, 11)
+            .padding(.bottom, 83)
         }
         .padding(.horizontal, 20)
+    }
+    
+    func separateDigits(_ number: Int) -> [Int] {
+        return String(number).compactMap { Int(String($0)) }
     }
 }
 
 #Preview {
-    OnboardingCompleteView()
+    OnboardingCompleteView(count: 123)
 }
