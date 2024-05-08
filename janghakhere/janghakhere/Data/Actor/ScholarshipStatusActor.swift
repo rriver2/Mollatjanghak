e27e26872c24aad9a9b03ef382e8a573f15f7984
rriver2 <rriver2@naver.com>
2024-05-09 03:13:41 +0900

    [Feat] getUserID() 추가

--- janghakhere/janghakhere/Data/Actor/ScholarshipStatusActor.swift
+++ janghakhere/janghakhere/Data/Actor/ScholarshipStatusActor.swift
@@ -1,5 +1,5 @@
 //
-//  SuccessFailActor.swift
+//  ScholarshipStatusActor.swift
 //  janghakhere
 //
 //  Created by Gaeun Lee on 5/3/24.
@@ -8,7 +8,6 @@
 import SwiftUI
 
 actor ScholarshipStatusActor {
-    @AppStorage("userData") private var userData: Data?
     
     struct scholarship: Encodable {
         let id: String
@@ -20,13 +19,13 @@ actor ScholarshipStatusActor {
         do {
             let postStruct = scholarship(id: id, status: status)
             
-            guard let userID = getUserID() else { throw URLError(.unknown) }
+            guard let userID = UserDateActor.getUserID() else { throw URLError(.unknown) }
             
             let (_, response) = try await HTTPUtils.postURL(postStruct: postStruct, urlBack: "/api/scholarships/\(id)/members/\(userID)/\(status)")
             
             switch response.statusCode {
             case 200:
-                print("성공")
+                
                 return true
             default: // 기술적 문제
                 throw URLError(.badServerResponse)
@@ -34,22 +33,5 @@ actor ScholarshipStatusActor {
         } catch {
             throw error
         }
-    }
-}
-
-extension ScholarshipStatusActor {
-    private func getUserID() -> String? {
-        if let data = userData {
-            do {
-                let decoder = JSONDecoder()
-                let loadedUserData = try decoder.decode(UserData.self, from: data)
-                
-                return loadedUserData.id
-            } catch {
-                print("Failed to decode user data: \(error)")
-                return nil
-            }
-        }
-        return nil
     }
 }
